f3c09570e353ec1c755d2df24499a6986717f908
wahyu.rizki <wahyu.rizki@accenture.com>
2024-12-30 13:43:02 +0700

    add API endpoint and logging middleware; update index page to fetch and display data

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
-//
+const { data, refresh } = await useFetch("/api/hello");
 </script>
 
 <template>
-  <VBtn> Button </VBtn>
+  <VBtn @click="refresh()"> Button </VBtn>
+  <pre>{{ data }}</pre>
 </template>
