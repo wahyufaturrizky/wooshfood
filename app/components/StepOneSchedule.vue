9a9a6dbe2f7973dd2f57764a3aff4e1b142ae039
wahyu.rizki <wahyu.rizki@accenture.com>
2025-01-28 01:21:54 +0700

    add HeaderSchedule and StepOneSchedule components for service selection and scheduling

---
+++ app/components/StepOneSchedule.vue
@@ -0,0 +1,140 @@
+<script setup lang="ts">
+const emit = defineEmits(["next", "update:regnumber"]);
+
+const service = ref();
+
+const tabPabel = [
+  {
+    title: "Basic Wash",
+    opt: [
+      {
+        title: "Wash - Large SUV",
+        price: 150,
+        time: 30,
+        desc: "Our SUV wash service offers a comprehensive cleaning experience tailored to larger vehicles. From exterior washing and waxing to interior vacuuming and detailing, we ensure your SUV looks its best inside and out.",
+      },
+      {
+        title: "Super Wash",
+        price: 150,
+        time: 30,
+        desc: "Our SUV wash service offers a comprehensive cleaning experience tailored to larger vehicles. From exterior washing and waxing to interior vacuuming and detailing, we ensure your SUV looks its best inside and out.",
+      },
+    ],
+  },
+  {
+    title: "Good Wash",
+    opt: [
+      {
+        title: "Wash - Large SUV",
+        price: 150,
+        time: 30,
+        desc: "Our SUV wash service offers a comprehensive cleaning experience tailored to larger vehicles. From exterior washing and waxing to interior vacuuming and detailing, we ensure your SUV looks its best inside and out.",
+      },
+      {
+        title: "Super Wash",
+        price: 150,
+        time: 30,
+        desc: "Our SUV wash service offers a comprehensive cleaning experience tailored to larger vehicles. From exterior washing and waxing to interior vacuuming and detailing, we ensure your SUV looks its best inside and out.",
+      },
+    ],
+  },
+  {
+    title: "Great Wash",
+    opt: [
+      {
+        title: "Wash - Large SUV",
+        price: 150,
+        time: 30,
+        desc: "Our SUV wash service offers a comprehensive cleaning experience tailored to larger vehicles. From exterior washing and waxing to interior vacuuming and detailing, we ensure your SUV looks its best inside and out.",
+      },
+      {
+        title: "Super Wash",
+        price: 150,
+        time: 30,
+        desc: "Our SUV wash service offers a comprehensive cleaning experience tailored to larger vehicles. From exterior washing and waxing to interior vacuuming and detailing, we ensure your SUV looks its best inside and out.",
+      },
+    ],
+  },
+];
+
+const { mdAndDown } = useDisplay();
+</script>
+
+<template>
+  <p class="text-black-500 text-lg mb-4">Select Your Service</p>
+
+  <VRow>
+    <VCol :cols="mdAndDown ? '12' : '6'">
+      <VExpansionPanels>
+        <VExpansionPanel v-for="({ title, opt }, index) in tabPabel" :key="index" :title="title">
+          <VExpansionPanelText>
+            <VRadioGroup v-model="service" color="#80509C">
+              <template v-for="({ title, price, time, desc }, subindex) in opt" :key="subindex">
+                <div :class="['rounded-lg p-6 mb-4']">
+                  <VRadio :value="title">
+                    <template #label>
+                      <div>
+                        <p class="text-base text-[#1D1F2C]">{{ title }}</p>
+                        <div class="mt-2">
+                          <div class="flex gap-2 items-center">
+                            <b class="text-xl text-[#1B223C]">$ {{ price }}</b>
+                            <b class="text-sm text-[#525B66] font-normal">{{ time }}</b>
+                          </div>
+                        </div>
+                        <p class="text-base text-[#4A4C56] mt-2">{{ desc }}</p>
+                      </div>
+                    </template>
+                  </VRadio>
+                </div>
+              </template>
+            </VRadioGroup>
+          </VExpansionPanelText>
+        </VExpansionPanel>
+      </VExpansionPanels>
+    </VCol>
+
+    <VCol :cols="mdAndDown ? '12' : '6'">
+      <VTable>
+        <thead>
+          <tr class="bg-purple-soft-woosh">
+            <th class="text-right rounded-tl-lg">Product</th>
+            <th class="text-right">Time</th>
+            <th class="text-right rounded-tr-lg">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="text-right">Wash - Large SUV</td>
+            <td class="text-right">30 Min</td>
+            <td class="text-right">$150.00</td>
+          </tr>
+          <tr>
+            <td class="text-right">Wash - Large SUV</td>
+            <td class="text-right">30 Min</td>
+            <td class="text-right">$150.00</td>
+          </tr>
+          <tr>
+            <td class="text-right">Wash - Large SUV</td>
+            <td class="text-right">30 Min</td>
+            <td class="text-right">$150.00</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colspan="2" class="text-right font-bold">Total</td>
+            <td class="text-right font-bold">$999.99</td>
+          </tr>
+        </tfoot>
+      </VTable>
+
+      <p class="font-normal text-sm text-[#777980]">
+        Prices are provided as estimates and are not fixed.
+      </p>
+      <p class="font-normal text-sm text-[#777980]">
+        Timing provided as estimates and are not fixed.
+      </p>
+    </VCol>
+  </VRow>
+
+  <div class="mt-4"><VBtn color="#80509C" block @click="emit('next')"> Next </VBtn></div>
+</template>
---
+++ app/pages/schedule/index.vue
@@ -0,0 +1,148 @@
+<script lang="ts" setup>
+import type { ResSalesOrder } from "~/interface/common.interface";
+
+const stepper = ref(1);
+
+const regNumber = ref("");
+const name = ref("");
+const email = ref("");
+const phone = ref("");
+const check = ref(false);
+const loading = ref(false);
+const plan = ref(2);
+const loyalPlan = ref();
+
+const cardType = ref("");
+const cardNumber = ref("");
+const expDate = ref("");
+const ccv = ref("");
+const paymentMethod = ref("credit-card");
+
+const stepperList = 4;
+
+const resetForm = () => {
+  regNumber.value = "";
+  name.value = "";
+  email.value = "";
+  phone.value = "";
+  check.value = false;
+};
+
+const handleProceed = async () => {
+  try {
+    loading.value = true;
+
+    const res: ResSalesOrder = await $fetch("/api/submit", {
+      method: "POST",
+      body: {
+        params: {
+          customer: {
+            name: name.value,
+            email: email.value,
+            phone: phone.value,
+            registration_number: regNumber.value,
+          },
+          date_order: formatDate(new Date()),
+          order_line: [],
+        },
+      },
+    });
+
+    if (res?.status === "success") {
+      const { msg } = res || {};
+      useSnackbar().sendSnackbar(msg, "success");
+      resetForm();
+      loading.value = false;
+    } else {
+      useSnackbar().sendSnackbar(res.msg, "error");
+      loading.value = false;
+    }
+  } catch (error) {
+    console.log("handleProceed", error);
+
+    loading.value = false;
+  }
+};
+
+const titleStep: { [key: number]: string } = {
+  1: "Services",
+  2: "Date & Time",
+  3: "Details",
+  4: "Complete",
+};
+</script>
+
+<template>
+  <div>
+    <HeaderSchedule />
+
+    <div class="mx-auto max-w-7xl py-11 sm:py-48 lg:py-56">
+      <div class="text-center">
+        <h1 class="text-balance text-5xl font-semibold tracking-tight text-black-500 sm:text-2xl">
+          Schedule Your Car Wash Quick & Easy!
+        </h1>
+      </div>
+
+      <VStepper v-model="stepper" class="mt-4" :flat="true" bg-color="transparent" alt-labels>
+        <template #default="{ next }">
+          <VStepperHeader class="!shadow-none">
+            <template v-for="n in stepperList" :key="`${n}-step`">
+              <VStepperItem
+                color="#80509C"
+                :value="n"
+                :step="titleStep[n]"
+                :title="titleStep[n]"
+                editable
+              />
+
+              <div v-if="n !== stepperList" :key="n" class="w-full border-dashed border" />
+            </template>
+          </VStepperHeader>
+
+          <VStepperWindow>
+            <VStepperWindowItem
+              v-for="n in stepperList"
+              :key="`${n}-content`"
+              :value="n"
+              class="p-4"
+            >
+              <div v-if="n === 1" class="mx-auto max-w-5xl">
+                <StepOneSchedule @update:regnumber="regNumber = $event.target.value" @next="next" />
+              </div>
+
+              <div v-if="n === 2" class="mx-auto max-w-xl bg-white p-6 rounded-lg shadow-lg">
+                <StepTwo
+                  @next="next"
+                  @update:name="name = $event.target.value"
+                  @update:email="email = $event.target.value"
+                  @update:phone="phone = $event.target.value"
+                  @update:check="check = $event.target.value"
+                />
+              </div>
+
+              <div v-if="n === 3" class="mx-auto max-w-4xl p-6">
+                <StepThree
+                  @update:loyalplan="loyalPlan = $event"
+                  @update:plan="plan = $event"
+                  @next="next"
+                />
+              </div>
+
+              <div v-if="n === 4">
+                <StepFour
+                  :loading="loading"
+                  @update:cardtype="cardType = $event"
+                  @update:cardnumber="cardNumber = $event"
+                  @update:expdate="expDate = $event"
+                  @update:ccv="ccv = $event"
+                  @update:paymentmethod="paymentMethod = $event"
+                  @proceed="handleProceed"
+                />
+              </div>
+            </VStepperWindowItem>
+          </VStepperWindow>
+        </template>
+      </VStepper>
+    </div>
+  </div>
+</template>
