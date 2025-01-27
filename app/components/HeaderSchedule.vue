491d8a307ed062b78e068e1f05dfcb8275bd99e7
wahyu.rizki <wahyu.rizki@accenture.com>
2025-01-28 02:10:45 +0700

    refactor HeaderSchedule component for improved responsive navigation layout

--- app/components/HeaderSchedule.vue
+++ app/components/HeaderSchedule.vue
@@ -1,21 +1,19 @@
 <template>
   <header class="shadow-md bg-white">
-    <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-      <div class="flex lg:flex-1 justify-evenly">
-        <NuxtImg height="25" width="136" src="/woosh-logo.png" alt="logo" />
-        <div class="items-center flex gap-8">
-          <p class="font-normal text-[#525B66] hover:text-purple-woosh cursor-pointer">Order Now</p>
-          <p class="font-normal text-[#525B66] hover:text-purple-woosh cursor-pointer">Booking</p>
-          <p class="font-normal text-[#525B66] hover:text-purple-woosh cursor-pointer">
-            Order History
-          </p>
-        </div>
-        <div class="items-center flex gap-4">
-          <NuxtImg height="24" width="24" src="/sample.png" alt="logo" />
-          <NuxtImg height="24" width="24" src="/sample.png" alt="logo" />
-          <NuxtImg height="24" width="24" src="/sample.png" alt="logo" />
-          <NuxtImg height="24" width="24" src="/sample.png" alt="logo" />
-        </div>
+    <nav class="flex items-center sm:justify-evenly p-6 lg:px-8" aria-label="Global">
+      <NuxtImg height="25" width="136" src="/woosh-logo.png" alt="logo" />
+      <div class="hidden sm:items-center sm:gap-4 sm:flex">
+        <p class="font-normal text-[#525B66] hover:text-purple-woosh cursor-pointer">Order Now</p>
+        <p class="font-normal text-[#525B66] hover:text-purple-woosh cursor-pointer">Booking</p>
+        <p class="font-normal text-[#525B66] hover:text-purple-woosh cursor-pointer">
+          Order History
+        </p>
+      </div>
+      <div class="hidden sm:items-center sm:gap-4 sm:flex">
+        <NuxtImg height="24" width="24" src="/sample.png" alt="logo" />
+        <NuxtImg height="24" width="24" src="/sample.png" alt="logo" />
+        <NuxtImg height="24" width="24" src="/sample.png" alt="logo" />
+        <NuxtImg height="24" width="24" src="/sample.png" alt="logo" />
       </div>
     </nav>
   </header>
