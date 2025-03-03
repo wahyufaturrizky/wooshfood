5bde4c1b9902074b295c578f4be93434e4432b72
wahyu.rizki <wahyu.rizki@accenture.com>
2025-03-03 14:40:23 +0700

    FIX: HeaderSchedule

--- app/components/HeaderSchedule.vue
+++ app/components/HeaderSchedule.vue
@@ -2,7 +2,7 @@
   <header class="shadow-md bg-white">
     <nav class="flex items-center sm:justify-evenly p-6 lg:px-8" aria-label="Global">
       <NuxtImg height="25" width="136" src="/woosh-logo.png" alt="logo" />
-      <div class="hidden sm:items-center sm:gap-4 sm:flex">
+      <!-- <div class="hidden sm:items-center sm:gap-4 sm:flex">
         <p class="font-normal text-[#525B66] hover:text-purple-woosh cursor-pointer">Order Now</p>
         <p class="font-normal text-[#525B66] hover:text-purple-woosh cursor-pointer">Booking</p>
         <p class="font-normal text-[#525B66] hover:text-purple-woosh cursor-pointer">
@@ -14,7 +14,7 @@
         <NuxtImg height="24" width="24" src="/favorite.png" alt="logo" />
         <NuxtImg height="24" width="24" src="/search.png" alt="logo" />
         <NuxtImg height="24" width="24" src="/profile.png" alt="logo" />
-      </div>
+      </div> -->
     </nav>
   </header>
 </template>
